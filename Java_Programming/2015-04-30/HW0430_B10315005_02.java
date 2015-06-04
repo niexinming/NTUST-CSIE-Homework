dd0af23221e0f13b6413a0e615af0848bc1ad5a7
Inndy <inndy.mail@gmail.com>
2015-06-04 14:15:32 +0800

    I am too lasy

---
+++ Java_Programming/2015-04-30/HW0430_B10315005_02.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class HW0430_B10315005_02
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I'm too lazy to do this practice.");
+    }
+}
