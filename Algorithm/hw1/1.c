a3628a3a4bdffa45725b138db2d7b14badaf3858
Inndy <inndy.tw@gmail.com>
2016-03-09 23:52:45 +0800

    Add comments

--- Algorithm/hw1/1.c
+++ Algorithm/hw1/1.c
@@ -4,17 +4,21 @@
 
 int data[100000];
 
+// 用來回傳結果的 structure
 typedef struct SEARCH_RESULT_ {
     int left;
     int right;
     int64_t value;
 } SEARCH_RESULT;
 
-void find_max_corssing_subarray(int l, int m, int h, SEARCH_RESULT *result)
+// 實作投影片上的程式
+void find_max_corssing_subarray(int low, int mid, int high, SEARCH_RESULT *result)
 {
     int64_t left_sum = INT64_MIN, right_sum = INT64_MIN, sum = 0;
-    int i, max_left = m, max_right = m;
-    for(i = m; i >= l; i--) {
+    int i, max_left = mid, max_right = mid;
+
+    // search left part
+    for(i = mid; i >= low; i--) {
         sum += data[i];
         if(sum > left_sum) {
             left_sum = sum;
@@ -22,35 +26,42 @@ void find_max_corssing_subarray(int l, int m, int h, SEARCH_RESULT *result)
         }
     }
 
+    // search right part
     sum = 0;
-    for(i = m + 1; i <= h; i++) {
+    for(i = mid + 1; i <= high; i++) {
         sum += data[i];
         if(sum > right_sum) {
             right_sum = sum;
             max_right = i;
         }
     }
+
     result->left = max_left;
     result->right = max_right;
     result->value = left_sum + right_sum;
 }
 
-void find_max_subarray(int l, int h, SEARCH_RESULT *result)
+void find_max_subarray(int low, int high, SEARCH_RESULT *result)
 {
-    if(l == h) {
-        result->left = l;
-        result->right = h;
-        result->value = data[l];
+    // 只剩一個的時候，最大值就是自己
+    if(low == high) {
+        result->left = low;
+        result->right = high;
+        result->value = data[low];
         return;
     }
-    int m = (l + h) / 2;
+
+    int mid = (low + high) / 2;
     SEARCH_RESULT left_result, right_result, cross_result;
+
     // search left to mid
-    find_max_subarray(l, m, &left_result);
+    find_max_subarray(low, mid, &left_result);
     // search mid+1 to right
-    find_max_subarray(m+1, h, &right_result);
+    find_max_subarray(mid+1, high, &right_result);
     // search cross mid result
-    find_max_corssing_subarray(l, m, h, &cross_result);
+    find_max_corssing_subarray(low, mid, high, &cross_result);
+
+    // return best result
     if(left_result.value >= right_result.value && left_result.value >= cross_result.value) {
         memcpy(result, &left_result, sizeof(SEARCH_RESULT));
     } else if(right_result.value >= left_result.value && right_result.value >= cross_result.value) {
@@ -63,11 +74,14 @@ void find_max_subarray(int l, int h, SEARCH_RESULT *result)
 int main(int argc, const char *argv[])
 {
     if(argc > 1) {
+        // 如果有提供執行參數
         if(freopen(argv[1], "r", stdin) == NULL) {
+            // 如果檔案開啟失敗
             fprintf(stderr, "Can not open input file\n");
             return 1;
         }
     } else {
+        // 如果沒有提供讀檔參數，則顯示提示訊息至 stderr
         fprintf(stderr, "** %s input-file.txt to read file, using stdin now.\n", argv[0]);
     }
 
@@ -78,11 +92,13 @@ int main(int argc, const char *argv[])
 
         SEARCH_RESULT result;
 
+        // 依序讀入所有資料
         for(int32_t i = 0; i < N; i++)
             scanf("%d", &data[i]);
 
         find_max_subarray(0, N-1, &result);
 
+        // 如果結果小於零，最佳解是全不取，等於零
         if(result.value < 0) {
             result.left = -1;
             result.right = -1;
@@ -92,3 +108,5 @@ int main(int argc, const char *argv[])
     }
     return 0;
 }
+
+// vim: sw=4
--- Algorithm/hw1/2.c
+++ Algorithm/hw1/2.c
@@ -4,14 +4,18 @@
 int main(int argc, const char *argv[])
 {
     if(argc > 1) {
+        // 如果有提供執行參數
         if(freopen(argv[1], "r", stdin) == NULL) {
+            // 如果檔案開啟失敗
             fprintf(stderr, "Can not open input file\n");
             return 1;
         }
     } else {
+        // 如果沒有提供讀檔參數，則顯示提示訊息至 stderr
         fprintf(stderr, "** %s input-file.txt to read file, using stdin now.\n", argv[0]);
     }
 
+    // 每筆測資第一個數字代表接下來有多少輸入
     int32_t N;
     while(scanf("%d", &N) != EOF) {
         if(N <= 0) break;
@@ -27,19 +31,24 @@ int main(int argc, const char *argv[])
         int64_t current_value = 0;
         int32_t current_left = 0, current_right = -1;
 
+        // O(N) solution!
         for(int32_t i = 0; i < N; i++) {
             scanf("%d", &input_val);
 
+            // 如果往右累加小於零則清掉current values（else做的事情）
             if(current_value + input_val > 0) {
+                // 累加 current values
                 current_value += input_val;
                 current_right = i;
 
+                // 刷新紀錄
                 if(current_value > max_value) {
                     max_value = current_value;
                     max_left = current_left;
                     max_right = current_right;
                 }
             } else {
+                // 清除 current values
                 current_left = i + 1;
                 current_right = i;
                 current_value = 0;
@@ -50,3 +59,5 @@ int main(int argc, const char *argv[])
     }
     return 0;
 }
+
+// vim: sw=4
